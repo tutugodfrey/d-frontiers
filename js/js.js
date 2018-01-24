d9e79a6c7365d87936043fe05246f18a38f1b094
tutu godfrey <godfrey_tutu@yahoo.com>
2018-01-24 17:41:39 +0100

    add content to index, about and services

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 function ini(){
 var date = document.LastModified;
-the_element = document.getElementById("copyright");
+the_element = document.getElementById("modify-notice");
 the_element.innerHTML += document.lastModified;
 }
